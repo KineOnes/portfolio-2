26f7e3e9afb144664d5db20d92e6397aae03116a
Kine Ones <kine.verhaaf@gmail.com>
2026-02-04 13:53:04 +0100

    Add projects to projects.js

---
+++ src/data/projects.js
@@ -0,0 +1,28 @@
+const projects = [
+    {
+      id: 1,
+      title: "Semester Project 2",
+      description: "E-commerce website built during first year at Noroff.",
+      image: "/assets/projects/semester-project-2.png",
+      repoUrl: "https://github.com/KineOnes/REPO-NAVN",
+      liveUrl: "https://live-site-url.com",
+    },
+    {
+      id: 2,
+      title: "JavaScript Frameworks CA",
+      description: "React-based project using API data.",
+      image: "/assets/projects/js-frameworks.png",
+      repoUrl: "https://github.com/KineOnes/REPO-NAVN",
+      liveUrl: "",
+    },
+    {
+      id: 3,
+      title: "Project Exam 2",
+      description: "Final exam project showcasing full-stack skills.",
+      image: "/assets/projects/project-exam-2.png",
+      repoUrl: "https://github.com/KineOnes/REPO-NAVN",
+      liveUrl: "",
+    },
+  ];
+  
+  export default projects;  
