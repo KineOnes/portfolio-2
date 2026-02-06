d58ffc753980f4a9489f592152d6a72439c12d83
Kine Ones <kine.verhaaf@gmail.com>
2026-02-06 12:12:01 +0100

    Create portfolio layout with sections and projects list

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,5 @@
-import ProjectsList from "./components/projects/ProjectsList";
+import Home from "./pages/Home";
 
 export default function App() {
-  return (
-    <main className="min-h-screen bg-purple-900 text-white p-8">
-      <h1 className="text-3xl font-bold mb-8">
-        My Projects
-      </h1>
-
-      <ProjectsList />
-    </main>
-  );
+  return <Home />;
 }
